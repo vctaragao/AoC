58091a261c76cc95a979bce52c0bace5fb165dcb
Victor Moraes <vct.aragao@gmail.com>
2023-12-16 22:56:51 -0300

    feat: day 1 part 2

--- 2023/day1/main.go
+++ 2023/day1/main.go
@@ -5,22 +5,26 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math"
 	"os"
 	"strconv"
 	"strings"
 	"unicode"
+
+	"golang.org/x/exp/maps"
+	"golang.org/x/exp/slices"
 )
 
 func main() {
-
 	input, err := os.Open("input.txt")
 	if err != nil {
 		log.Fatalln("error opening input file", err)
 	}
 	defer input.Close()
 
-	finalValues := []int64{}
+	finalValues := []int{}
 	reader := bufio.NewReader(input)
+
 	for {
 		line, err := reader.ReadString('\n')
 		if err != nil {
@@ -31,37 +35,110 @@ func main() {
 			log.Fatalln("error reading line from file: ", err)
 		}
 
-		last := ""
+		mappedDigits := findDigits(line)
+		mappedStrDigits := strDigits(line)
+
+		if len(mappedDigits) == 0 && len(mappedStrDigits) == 0 {
+			continue
+		}
+
+		minDigitIndex, maxDigitIndex := getMaxAndMin(mappedDigits)
+		minStrDigitIndex, maxStrDigitIndex := getMaxAndMin(mappedStrDigits)
+
 		first := ""
-		digits := []string{}
+		last := ""
 
-		for i := 0; i < len(line); i++ {
-			if unicode.IsDigit(rune(line[i])) {
-				if first == "" {
-					first = string(line[i])
-					digits = append(digits, string(line[i]))
-				}
-
-				last = string(line[i])
-			}
+		if minStrDigitIndex < minDigitIndex {
+			first = mappedStrDigits[minStrDigitIndex]
+		} else {
+			first = mappedDigits[minDigitIndex]
 		}
 
-		digits = append(digits, last)
+		if maxStrDigitIndex > maxDigitIndex {
+			last = mappedStrDigits[maxStrDigitIndex]
+		} else {
+			last = mappedDigits[maxDigitIndex]
+		}
 
-		// join the first digit and the last digit into a single value
-		value, err := strconv.Atoi(strings.Join(digits, ""))
+		value, err := strconv.Atoi(first + last)
 		if err != nil {
-			log.Fatalln("error joning digits into a value: ", err)
+			log.Fatalln("error joining digits into a value: ", err)
 		}
 
-		finalValues = append(finalValues, int64(value))
+		finalValues = append(finalValues, value)
 	}
 
-	// add those values to get the final answer
-	result := int64(0)
+	result := 0
 	for _, v := range finalValues {
 		result += v
 	}
 
 	fmt.Println("Result: ", result)
+}
+
+func getMaxAndMin(mappedValues map[int]string) (int, int) {
+	maxValue := math.MinInt
+	minValue := math.MaxInt
+
+	if len(mappedValues) == 0 {
+		return minValue, maxValue
+	}
+
+	digitIndexes := maps.Keys(mappedValues)
+	return slices.Min(digitIndexes), slices.Max(digitIndexes)
+}
+
+func strDigits(line string) map[int]string {
+	strDigits := []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+
+	srtDigitsFound := map[int]string{}
+	for _, strDigit := range strDigits {
+		first := strings.Index(line, strDigit)
+		if first == -1 {
+			continue
+		}
+
+		last := strings.LastIndex(line, strDigit)
+
+		srtDigitsFound[first] = converStrDigitToDigit(strDigit)
+		srtDigitsFound[last] = converStrDigitToDigit(strDigit)
+	}
+
+	return srtDigitsFound
+}
+
+func findDigits(line string) map[int]string {
+	digits := map[int]string{}
+	for i, c := range line {
+		if unicode.IsDigit(c) {
+			digits[i] = string(c)
+		}
+	}
+
+	return digits
+}
+
+func converStrDigitToDigit(strDigit string) string {
+	switch strDigit {
+	case "one":
+		return "1"
+	case "two":
+		return "2"
+	case "three":
+		return "3"
+	case "four":
+		return "4"
+	case "five":
+		return "5"
+	case "six":
+		return "6"
+	case "seven":
+		return "7"
+	case "eight":
+		return "8"
+	case "nine":
+		return "9"
+	default:
+		return ""
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/vctaragao/advent_of_code
 
 go 1.19
+
+require golang.org/x/exp v0.0.0-20231214170342-aacd6d4b4611
